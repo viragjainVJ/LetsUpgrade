03874a5c92cad6b613df050713c1cf8aae112fdc
Virag Jain <virag.j.jain@oracle.com>
2020-07-22 15:47:12 +0530

    Day5 | Batch1

---
+++ JavaScript/Day5/question2.js
@@ -0,0 +1,80 @@
+class User {
+    constructor(name, age,email) {
+      this.name = name;
+      this.age = age;
+      this.email = email;
+      this.luCoins = 0;
+      this.courses = [];
+    }
+    login(){
+        console.log(`${this.name} has logged in`);
+        return this;
+    }
+    logout(){
+        console.log(`${this.name} has logged out`);
+        return this;
+    }
+}
+
+class Moderator extends User{
+    constructor(name,age,email,role){
+        super(name,age,email);
+        this.role = role;
+    }
+
+    addCoins(user){
+        user.luCoins++;
+        console.log(`${user.name} has ${user.luCoins} coins`);
+        return this;
+    }
+    removeCoins(user){
+        if(user.luCoins){
+            user.luCoins--;
+            console.log(`${user.name} has ${user.luCoins} coins`);
+        } else {
+            console.error(`${user.name} has ${user.luCoins} coins`);
+        }
+        return this;
+    }
+
+}
+
+class Admin extends Moderator{
+   addCourse(user, course){
+       user.courses.push(course);
+       console.log(user.courses)
+       return this;
+   }
+   deleteCourse(user, course){
+       if(user.courses.includes(course))
+       {
+           user.courses = user.courses.reduce((acc, val)=>{
+            if(val != course)    
+            acc.push(val)
+            return acc;
+           },[]);
+           console.log(user.courses)
+       }
+       return this;
+   }
+}
+
+
+let user1 = new User('Tom',25,'tom@gmail.com')
+let user2 = new User('Harry',24,'harry@gmai.com')
+let mod = new Moderator('Avenger',24,'av@gmail.com','Moderator');
+let admin = new Admin('Disney',25,'d@gmail.com', 'Admin');
+let users = [user1, user2, mod, admin];
+
+let user = users[0];
+user.login();
+console.log(user)
+mod.addCoins(user).addCoins(user1)
+mod.removeCoins(user).removeCoins(user1).addCoins(user)
+
+admin.addCourse(user,'Javascript');
+admin.addCourse(user,'Python');
+
+admin.deleteCourse(user,'Javascript');
+console.log(user)
+user.logout()
